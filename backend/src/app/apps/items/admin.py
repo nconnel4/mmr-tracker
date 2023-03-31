ae2077216fb9212db4f29dd4f7ce69132fb2ae6f
nconnel4 <nconnel4@hotmail.com>
2023-03-31 16:26:15 -0400

    chore: update admin items display

--- backend/src/app/apps/items/admin.py
+++ backend/src/app/apps/items/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Item
 
-admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    fields = ("code", "name", "item_group", "progress_order", "item_type", "quantity")
+    list_display = ("code", "name", "item_group", "item_type")
